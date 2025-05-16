870aa9a60d170baf8661093076bc69c3eb387ab0
LayzaPelinson <rm95322@estudante.fieb.edu.br>
2025-05-16 12:59:23 +0000

    flex

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,24 @@
-
+import poke from './assets/pokeapi_256.png'
+import eevee from './assets/eevee.jpg'
+import './Home.css'
 
 const Home =()=>{
     return (
         <div>
-            <p>Routes blood Routes</p>
+            <img scr={poke} />
+            <img scr={eevee} />
+            <img scr={poke} />
+            <img scr={eevee} />
+            <img scr={poke} />
+            <img scr={eevee} />
+            <img scr={poke} />
+            <img scr={eevee} />
+            <img scr={poke} />
+            <img scr={eevee} />
+            <img scr={poke} />
+            <img scr={eevee} />
         </div>
-{
-
+    )
+}
 
 export default Home
