edac4371881e81bc00dfe5b3f064288485e97683
LayzaPelinson <rm95322@estudante.fieb.edu.br>
2025-05-23 12:53:34 +0000

    flex

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,14 +3,10 @@ import eevee from './assets/eevee.jpg'
 import './Home.css'
 
 const Home =()=>{
+
+
     return (
         <div>
-            <img scr={poke} />
-            <img scr={eevee} />
-            <img scr={poke} />
-            <img scr={eevee} />
-            <img scr={poke} />
-            <img scr={eevee} />
             <img scr={poke} />
             <img scr={eevee} />
             <img scr={poke} />
